34cc7ef9ca6fe1d1a501c8a8d5bab712d3511bfd
Higor Matos <higordeus22@gmail.com>
2022-06-01 17:42:15 -0300

    Agora é possível baixar as listas da internet

--- App/src/App.java
+++ App/src/App.java
@@ -6,20 +6,21 @@ public class App {
         String Entrada;
         UrlHttp urlhttp = new UrlHttp();
         UrlHttps urlhttps = new UrlHttps();
-
         do{
+
             System.out.print("Insira a Url para verificar confiabilidade.\nExemplo: https://www.google.com.br/\nUrl: ");
             Entrada = scanner.next();
 
             if (Entrada.contains("https")) {//Verifico o protocolo do endereco
                 if(urlhttps.validaUrl(Entrada)){//Chamo a função para verificar se o endereco é valido
-                    urlhttps.Endereco = Entrada;//Quando válido, coloco na varíavel do objeto
-                    System.out.println("É válida " + urlhttps.Endereco);
+                    urlhttps.setEndereco(Entrada);//Quando válido, coloco na varíavel do objeto
+                    System.out.println("É válida " + urlhttps.getEndereco());
+                    
                 }
             } else if (Entrada.contains("http")) {
                 if(urlhttp.validaUrl(Entrada)){
-                    urlhttp.Endereco = Entrada;
-                    System.out.println("É válida " + urlhttp.Endereco);
+                    urlhttp.setEndereco(Entrada);
+                    System.out.println("É válida " + urlhttp.getEndereco());
                 }
             } else {
                 System.out.println(">>> URL inválida <<< \nVerifique se inseriu corretamente com o protocolo HTTP ou HTTPS");
